b68228c21f6ddd8889666d336e67ca1da341b746
Andrew Jorczak <andrew.jorczak@gmail.com>
2020-10-14 20:39:06 -0700

    Update main.py.

--- app/main.py
+++ app/main.py
@@ -17,7 +17,7 @@ load_dotenv()
 
 import os
 
-DATABASE_URL =  os.getenv('DATABASE_URL')
+DATABASE_URL = os.getenv('DATABASE_URL')
 SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG= True
 CSRF_ENABLED = True
